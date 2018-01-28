3fb75028251338d9703f78a23a6682518a4517e4
raajeshsr <srraajesh97@gmail.com>
2018-01-28 22:28:22 +0530

    improved code modularity and readabilty

--- quiz/urls.py
+++ quiz/urls.py
@@ -4,9 +4,5 @@ from django.contrib import admin
 from . import views
 app_name="quiz"
 urlpatterns = [
-    url(r'^(?P<pk>[0-9]+)$',views.index,name="index"),
-    url(r'^next/(?P<pk>[0-9]+)$',views.nextQuestion,name="nextQuestion"),
-    url(r'^previous/(?P<pk>[0-9]+)$',views.previousQuestion,name="previousQuestion"),
-    
-    
+    url(r'^(?P<pk>[0-9]+)$',views.index,name="index")
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -16,72 +16,105 @@ last_question=questions[length-1].id
 
 
 @login_required(login_url="/")  
+def index(request,pk):
+	current_user=request.user
+	username=current_user.username
+	if(request.method=='POST'):		
+
+		if(request.POST['nav']=='next'):
+			pk=nextQuestion(request,pk)
+
+		if(request.POST['nav']=='previous'):
+			pk,saved_choice=previousQuestion(request,pk)
+			question=Question.objects.get(pk=pk)	
+			return render(request,'quiz/index.html',{'question':question,'first_question':first_question,'last_question':last_question,'saved_choice':saved_choice,'username':username})
+
+		if(request.POST['nav']=='submit'):
+			mark=result(request,pk)
+			return render(request,'quiz/result.html',{'mark':mark,'username':username})
+
+	question=Question.objects.get(pk=pk)	
+	return render(request,'quiz/index.html',{'question':question,'first_question':first_question,'last_question':last_question,'username':username})
+
 def previousQuestion(request,pk):
 	current_user=request.user
 	user=UserProfile.objects.get(user=current_user)
-	count=Question.objects.filter(pk__lt=pk).count()
-	user.current_question=Question.objects.filter(pk__lt=pk)[count-1].id
+
+	#getting previous question no
+	question_list=Question.objects.filter(pk__lt=pk)
+	question_list_length=question_list.count()
+	user.current_question=Question.objects.filter(pk__lt=pk)[question_list_length-1].id
 	user.save()
 	
-	#getting previous question option
 	question=Question.objects.get(pk=user.current_question)
 	saved_choice=UserChoice.objects.get(user=current_user,question=question)
-	
-	
-	if question.choice_set.get(choice_text=saved_choice).correct_choice:
+
+	#reducing mark if previous ans is correct 
+	previous_choice=question.choice_set.get(choice_text=saved_choice)
+	if previous_choice.correct_choice:
 		decrementMark(user)
 
-	return render(request,'quiz/index.html',{'question':question,'first_question':first_question,'last_question':last_question,'saved_choice':saved_choice.choice_text})
 
 
-def isLast(pk):
-	pk=int(pk)
-	if pk==last_question:
-		return True
-	else: 
-		return False	
+	pk=user.current_question
+	
+	saved_choice=saved_choice.choice_text	
 
+	return pk,saved_choice
 
-@login_required(login_url="/")  
-def index(request,pk):
-
-	question=Question.objects.get(pk=pk)	
-	return render(request,'quiz/index.html',{'question':question,'first_question':first_question,'last_question':last_question})
-
-
-@login_required(login_url="/")  
 def nextQuestion(request,pk):
 	
 	question=Question.objects.get(pk=pk)
 	
-	selected_choice= request.GET['choice']
+	selected_choice= request.POST['choice']
 	
 	current_user=request.user
 
-	choice=question.choice_set.get(choice_text=selected_choice)
+	selectedChoice=question.choice_set.get(choice_text=selected_choice)
 	
 	user=UserProfile.objects.get(user=current_user)
 	
 	saveChoice(current_user,question,selected_choice)
 
+	validate(selectedChoice,user)
+	
+	
+	user.current_question=Question.objects.filter(pk__gt=pk)[0].id
+	
+	user.save()		
+	
+	pk=user.current_question
 
-	if choice.correct_choice:
+	return pk
+
+def result(request,pk):
+	current_user=request.user
+
+	selected_choice= request.POST['choice']
+
+	question=Question.objects.get(pk=pk)
+		
+	selectedChoice=question.choice_set.get(choice_text=selected_choice)
+
+	user=UserProfile.objects.get(user=current_user)
+
+	validate(selectedChoice,user)
+
+	saveChoice(current_user,question,selected_choice)
+
+	deactivateUser(current_user)
+
+	mark = user.mark
+
+	return mark
+	
+def validate(selectedChoice,user):
+
+	if selectedChoice.correct_choice:
 		
 		incrementMark(user)
 
-	if isLast(pk):	
-		
-		deactivateUser(current_user)
 
-		return render(request,'quiz/result.html',{'mark':user.mark})
-	
-	else:	
-	
-		user.current_question=Question.objects.filter(pk__gt=pk)[0].id
-	
-		user.save()	
-	
-		return redirect('quiz:index',pk=user.current_question)	
 
 
 def saveChoice(current_user,question,selected_choice):
